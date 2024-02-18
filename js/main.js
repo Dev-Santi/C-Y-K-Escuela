3a113ca623990cfb49bbe531b16ccc551a75bc2c
dev-santi <bauzalarrosasantiago@gmail.com>
2024-02-18 19:09:32 -0300

    Actualizacion

--- js/main.js
+++ js/main.js
@@ -4,20 +4,23 @@ const indiceFondoFinal = 2;
 let indicieFondoActual = indiceFondoFinal;
 
 // Programa
-window.addEventListener('load', programa);
+window.addEventListener("load", programa);
 
 function programa() {
     //Constantes
-    const body = document.getElementById('idBody');
-    const menu = document.getElementById('idMenu');
-    const botonMenu = document.getElementById('idBotonDeNavegacion');
-    const contenedorDeOpacidad = document.getElementById('idContenedorDeOpacidad');
-    const contenedorFondos = document.getElementById('idContenedorDeFondos');
+    const body = document.getElementById("idBody");
+    const menu = document.getElementById("idMenu");
+    const botonMenu = document.getElementById("idBotonDeNavegacion");
+    const contenedorDeOpacidad = document.getElementById(
+        "idContenedorDeOpacidad"
+    );
+    const contenedorFondos = document.getElementById("idContenedorDeFondos");
 
     //Botón para desplegar el menú de navegación
-    botonMenu.addEventListener('click', abrirYCerrarMenu);
+    botonMenu.addEventListener("click", abrirYCerrarMenu);
     for (let link of menu.children) {
-        link.addEventListener('click', abrirYCerrarMenu);
+        console.log(link);
+        link.addEventListener("click", abrirYCerrarMenu);
     }
 
     //Desplazamiento de fondos en el inicio
@@ -27,19 +30,21 @@ function programa() {
 
     //Funciones
     function abrirYCerrarMenu() {
-        menu.classList.toggle('visible');
-        botonMenu.classList.toggle('cerrar');
-        contenedorDeOpacidad.classList.toggle('opacidad_activada');
-        //Desactivar el scroll cuando el menu este activo
-        body.classList.toggle('no_scroll');
+        menu.classList.toggle("visible");
+        botonMenu.classList.toggle("cerrar");
+        contenedorDeOpacidad.classList.toggle("opacidad_activada");
+        //Desactivar el scroll cuando el menu este activo, solo cuando se esta en modo mobile
+        if (window.innerWidth < 896) {
+            body.classList.toggle("no_scroll");
+        }
     }
 
     function animacionImagenesInicio() {
         for (let fondo of contenedorFondos.children) {
             if (fondo.id.includes(indicieFondoActual + 1)) {
-                fondo.classList.remove('ocultar');
+                fondo.classList.remove("ocultar");
             } else {
-                fondo.classList.add('ocultar');
+                fondo.classList.add("ocultar");
             }
         }
 
